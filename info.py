cebc4b258592fcb81fcc3e222e39c1bbc700d10a
Sureshssd77 <sureshjaat33772@gmail.com>
2025-01-03 16:28:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,66 +11,49 @@ def is_enabled(value, default):
     elif value.strip().lower() in ["off", "false", "no", "0", "disable", "n"]: return False
     else: return default
 
-# .env से जानकारी लोड करने के लिए dotenv module का उपयोग करें
-from dotenv import load_dotenv
-load_dotenv()
-
-# API Credentials और Bot details
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+# API Credentials और Bot details (जो आपने दी थी)
+API_ID = 22506926  # API ID यहाँ डाला गया है
+API_HASH = '34fe7b7d19572aae39c6db80e151d9f7'  # API HASH यहाँ डाला गया है
+BOT_TOKEN = '8004078816:AAFpANlxyG1emrSLYYuJXgEecNGJIvme78c'  # BOT TOKEN यहाँ डाला गया है
 
 # MongoDB details
-DATABASE_URL = environ.get('DATABASE_URL', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-FILE_DB_URL = environ.get("FILE_DB_URL", DATABASE_URL)
-FILE_DB_NAME = environ.get("FILE_DB_NAME", DATABASE_NAME)
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URL = "mongodb+srv://dilipdewasi7759:<NMzFkZqWZdi1GDG4>@ssdmovies.2nqad.mongodb.net/?retryWrites=true&w=majority&appName=ssdmovies"
+DATABASE_NAME = "Cluster0"
+FILE_DB_URL = DATABASE_URL
+FILE_DB_NAME = DATABASE_NAME
+COLLECTION_NAME = 'Telegram_files'
 
 # Admins, Channels & Users
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6151975257').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002144001843').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
-auth_grp = environ.get('AUTH_GROUP')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+CACHE_TIME = 300
+ADMINS = [6151975257]  # Admin ID यहाँ डाला गया है
+CHANNELS = [-1002144001843]  # Channel ID यहाँ डाला गया है
+AUTH_USERS = [6151975257]  # Auth Users
+AUTH_CHANNEL = None
+AUTH_GROUPS = None
 
 # Welcome image and message
-WELCOM_PIC = environ.get("WELCOM_PIC", "https://graph.org/file/1118970f6a47013b7ec47-1903a973000f540eca.jpg")
-WELCOM_TEXT = environ.get("WELCOM_TEXT", script.WELCOM_TEXT)
+WELCOM_PIC = "https://graph.org/file/1118970f6a47013b7ec47-1903a973000f540eca.jpg"
+WELCOM_TEXT = script.WELCOM_TEXT
 
 # Bot settings
-WEB_SUPPORT = bool(environ.get("WEBHOOK", 'True')) # for web support on/off
-PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
+WEB_SUPPORT = True
+PICS = [
+    'https://graph.org/file/01ddfcb1e8203879a63d7.jpg',
+    'https://graph.org/file/d69995d9846fd4ad632b8.jpg',
+    'https://graph.org/file/a125497b6b85a1d774394.jpg'
+]
 UPTIME = time.time()
 
 # Filters Configuration 
-MAX_RIST_BTNS = int(environ.get('MAX_RIST_BTNS', "10"))
-START_MESSAGE = environ.get('START_MESSAGE', script.START_TXT)
-BUTTON_LOCK_TEXT = environ.get("BUTTON_LOCK_TEXT", script.BUTTON_LOCK_TEXT)
-FORCE_SUB_TEXT = environ.get('FORCE_SUB_TEXT', script.FORCE_SUB_TEXT)
+MAX_RIST_BTNS = 10
+START_MESSAGE = script.START_TXT
+BUTTON_LOCK_TEXT = script.BUTTON_LOCK_TEXT
+FORCE_SUB_TEXT = script.FORCE_SUB_TEXT
 
 # Others
-IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MKN_BOTZ_DISCUSSION_GROUP')
-P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', "True"), True)
-PM_IMDB = is_enabled(environ.get('PM_IMDB', "True"), True)
-IMDB = is_enabled(environ.get('IMDB', "True"), True)
-SINGLE_BUTTON = is_enabled(environ.get('SINGLE_BUTTON', "True"), True)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "{file_name}")
-BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", None)
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
-LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
-SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
-MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
-MELCOW_NEW_USERS = is_enabled(environ.get('MELCOW_NEW_USERS', "True"), True)
-PROTECT_CONTENT = is_enabled(environ.get('PROTECT_CONTENT', "False"), False)
-PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "True"), True)
+IMDB_DELET_TIME = 300
+LOG_CHANNEL = -1002429570370  # Log Channel ID यहाँ डाला गया है
+SUPPORT_CHAT = 'MKN_BOTZ_DISCUSSION_GROUP'
 
 # Pyrogram client setup
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
