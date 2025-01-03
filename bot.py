83a915f365a758a04f50423dcf7058d4118f488b
Sureshssd77 <sureshjaat33772@gmail.com>
2025-01-03 16:53:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,79 +1,71 @@
-import os, math, logging, datetime, pytz, logging.config
+# bot.py
 
-from aiohttp import web
-from pyrogram import Client, types
-from database.users_chats_db import db
-from database.ia_filterdb import  Media
-from typing import Union, Optional, AsyncGenerator
-from utils import temp, __repo__, __license__, __copyright__, __version__
-from info import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, UPTIME, WEB_SUPPORT, LOG_MSG
+import re, time
+from os import environ
+from info import DATABASE_NAME, DATABASE_URL, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT, MAX_RIST_BTNS, IMDB_DELET_TIME
+from Script import script
 
-# Get logging configurations
-logging.config.fileConfig("logging.conf")
-logging.getLogger(__name__).setLevel(logging.INFO)
-logging.getLogger("cinemagoer").setLevel(logging.ERROR)
+id_pattern = re.compile(r'^.\d+$')
 
+def is_enabled(value, default):
+    if value.strip().lower() in ["on", "true", "yes", "1", "enable", "y"]: return True
+    elif value.strip().lower() in ["off", "false", "no", "0", "disable", "n"]: return False
+    else: return default
 
-class Bot(Client):
-    def __init__(self):
-        super().__init__(
-            name="Professor-Bot",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            plugins=dict(root="plugins")
-        )
+# PyroClient Setup 
+API_ID = 22506926
+API_HASH = "34fe7b7d19572aae39c6db80e151d9f7"
+BOT_TOKEN = "8004078816:AAFpANlxyG1emrSLYYuJXgEecNGJIvme78c"
 
-    async def start(self):
-        b_users, b_chats = await db.get_banned()
-        temp.BANNED_USERS = b_users
-        temp.BANNED_CHATS = b_chats        
-        
-        await super().start()
-        await Media.ensure_indexes()
-        me = await self.get_me()
-        temp.U_NAME = me.username
-        temp.B_NAME = me.first_name
-        self.id = me.id
-        self.name = me.first_name
-        self.mention = me.mention
-        self.username = me.username
-        self.log_channel = LOG_CHANNEL
-        self.uptime = UPTIME
-        curr = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
-        date = curr.strftime('%d %B, %Y')
-        tame = curr.strftime('%I:%M:%S %p')
-        logging.info(LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__))
-        
-        try: await self.send_message(LOG_CHANNEL, text=LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__), disable_web_page_preview=True)   
-        except Exception as e: logging.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
-        
-        if bool(WEB_SUPPORT) is True:
-            app = web.AppRunner(web.Application(client_max_size=30000000))
-            await app.setup()
-            await web.TCPSite(app, "0.0.0.0", 8080).start()
-            logging.info("Web Response Is Running......🕸️")
-            
-    async def stop(self, *args):
-        await super().stop()
-        logging.info(f"Bot Is Restarting ⟳...")
+# Bot settings
+WEB_SUPPORT = True # for web support on/off
+PICS = "https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg".split()
+UPTIME = time.time()
 
-    async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:                       
-        current = offset
-        while True:
-            new_diff = min(200, limit - current)
-            if new_diff <= 0:
-                return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
-            for message in messages:
-                yield message
-                current += 1
+# Admins, Channels & Users
+CACHE_TIME = 300
+ADMINS = [6151975257]  # Admin ID
+CHANNELS = [-1002144001843]  # Channel ID
+auth_users = [6151975257]
+AUTH_USERS = auth_users + ADMINS
+AUTH_CHANNEL = None
+AUTH_GROUPS = None
 
+# MongoDB information
+DATABASE_URL = "mongodb+srv://dilipdewasi7759:<NMzFkZqWZdi1GDG4>@ssdmovies.2nqad.mongodb.net/?retryWrites=true&w=majority&appName=ssdmovies"
+FILE_DB_URL = DATABASE_URL
+FILE_DB_NAME = DATABASE_NAME
+COLLECTION_NAME = "Telegram_files"
 
-        
-Bot().run()
+# Filters Configuration 
+MAX_RIST_BTNS = 10
+START_MESSAGE = script.START_TXT
+BUTTON_LOCK_TEXT = script.BUTTON_LOCK_TEXT
+FORCE_SUB_TEXT = script.FORCE_SUB_TEXT
 
+WELCOM_PIC = "https://graph.org/file/1118970f6a47013b7ec47-1903a973000f540eca.jpg"
+WELCOM_TEXT = script.WELCOM_TEXT
+PMFILTER = True
+G_FILTER = True
+BUTTON_LOCK = True
+RemoveBG_API = ""
 
-
-
-
+# Others
+IMDB_DELET_TIME = 300
+LOG_CHANNEL = -1002429570370  # Log Channel ID
+SUPPORT_CHAT = 'MKN_BOTZ_DISCUSSION_GROUP'
+P_TTI_SHOW_OFF = True
+PM_IMDB = True
+IMDB = True
+SINGLE_BUTTON = True
+CUSTOM_FILE_CAPTION = "{file_name}"
+BATCH_FILE_CAPTION = None
+IMDB_TEMPLATE = script.IMDB_TEMPLATE
+LONG_IMDB_DESCRIPTION = False
+SPELL_CHECK_REPLY = True
+MAX_LIST_ELM = None
+FILE_STORE_CHANNEL = [-1002144001843]  # File Store Channel
+MELCOW_NEW_USERS = True
+PROTECT_CONTENT = False
+PUBLIC_FILE_STORE = True
+LOG_MSG = "{} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ....✨\n\n🗓️ Dᴀᴛᴇ : {}\n⏰ Tɪᴍᴇ : {}\n\n🖥️ Rᴇᴏᴩ: {}\n🉐 Vᴇʀsɪᴏɴ: {}\n🧾 Lɪᴄᴇɴꜱᴇ: {}\n©️ Cᴏᴩʏʀɪɢʜᴛ: {}"
